8b60ebd5aaab8a7556021c1518dec0baa8354de0
Minhaj <minhajsinanvp@gmail.com>
2023-09-23 10:29:05 +0530

    working on section-3

--- statics/js/myscript.js
+++ statics/js/myscript.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     var header = document.querySelector('.header-wrapper');
-    if (window.scrollY > 200) { // Adjust the scroll threshold as needed
+    if (window.scrollY > 100) { // Adjust the scroll threshold as needed
         header.classList.add('navbar-scrolled');
     } else {
         header.classList.remove('navbar-scrolled');
